63e6098faf7f3cce58c3d84127fe2a9a5c3ddf79
espinosa012 <espinosa012@gmail.com>
2026-03-09 10:19:03 +0100

    async implementation

--- bswebpilot/utils/page.py
+++ bswebpilot/utils/page.py
@@ -1,17 +1,18 @@
+from bsutils.logger.bslogger import BSLogger
+
 from bswebpilot.base import BSWebPilotSync, BSWebPilotAsync
 
 
-class BSLogger:
-    pass
-
-
 class BSWebPilotPage:
+    pilot: BSWebPilotSync | BSWebPilotAsync
+    logger: BSLogger
+
     def __init__(self, pilot:BSWebPilotSync | BSWebPilotAsync, logger: BSLogger):
         """
         Clase base para todas las páginas (síncrono o asíncrono).
         """
-        pilot: BSWebPilotSync | BSWebPilotAsync
-        logger: BSLogger
+        self.pilot = pilot
+        self.logger = logger
 
 
 
